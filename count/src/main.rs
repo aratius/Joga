3160b9f531beb07a957d97cf8913583c05a0fc37
aratius <arata1129matsu@icloud.com>
2021-11-03 19:40:17 +0900

    乱数生成

--- count/src/main.rs
+++ count/src/main.rs
@@ -1,8 +1,14 @@
 use std::io;  // ユーザ入力を受け付ける能力などの実用的な機能の多くを使用することができる
+use rand::Rng;
 
 // ユーザに予想を入力してもらい、それを出力するコード
 fn main() {
     println!("Guess the number");
+
+    // 乱数を生成
+    // rand::thread_rng -> 乱数生成器を返却
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secrest number is: {}", secret_number);
 
     println!("Please input your guess");
 
